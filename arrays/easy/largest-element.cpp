231b52d5f1571e039cfcc3e6b8a22f5ef648205b
sumanthd032 <sumanthd032@gmail.com>
2025-09-04 23:22:42 +0530

    arrays setup

---
+++ arrays/easy/largest-element.cpp
@@ -0,0 +1,24 @@
+// Given an array, we have to find the largest element in the array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int largest(vector<int>& arr){
+    int max = arr[0];
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    cout << "Largest element is " << largest(arr) << endl;
+    return 0;
+}
